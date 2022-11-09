f1e21b0727971354766b309843fdd50d9ff3def6
Neha Singh <nehasingh2mau@gmail.com>
2022-11-10 03:11:34 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,35 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoute.js';
+import carsRouter from './routes/carsRoute.js';
+import userRouter from './routes/userRoute.js';
+import rentRouter from './routes/rentRoute.js';
+import categoryRouter from './routes/typeRoute.js';
+
 const app = express();
-const port = process.env.PORT || 5002;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed/', seedRouter);
+app.use('/api/cars/', carsRouter);
+app.use('/api/users/', userRouter);
+app.use('/api/rent/', rentRouter);
+app.use('/api/category/', categoryRouter);
+
+//Connect to DB
+dotenv.config();
+mongoose.connect("mongodb+srv://nikhilverma360:nikhilop1234@cluster0.2wndq7w.mongodb.net/?retryWrites=true&w=majority").then(() => {
+
+    console.log("Connected to DB");
+
+}).catch((error) => {
+    console.log(error.message);
+})
+
+//Create Port
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
     console.log(`Serve at: http://localhost:${port}`);
 })
