e2f1b6beac5cc330bf8b2f4ddb44c704294aa286
Neha Singh <nehasingh2mau@gmail.com>
2022-11-10 03:09:31 +0530

    added db models

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 app.listen(port, () => {
     console.log(`Serve at: http://localhost:${port}`);
 })
